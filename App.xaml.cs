82f09c12bbef8b86f99c49a52d5e5ab7ae4d7aef
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-06-17 16:44:15 -0500

    set up project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GuevaraGCreacionAppApuntes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GuevaraGCreacionAppApuntes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
